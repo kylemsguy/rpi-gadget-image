980294ce20773d095606215125d2e5326dc45bba
Marco Attia <54147992+Vaneixus@users.noreply.github.com>
2023-10-01 22:43:18 -0400

    Default to x64 bit by default, Download image if not provided, Install code-server, configure USB Gadget for Serial and Linux Ethernet, Install Network Manager, pre-configure the image with user credentials.

--- setup.sh
+++ setup.sh
@@ -1,31 +1,86 @@
 #!/bin/bash
 
+RASPIAN_OS_FILE="$1"
+
+if [[ $(id -u) -ne 0 ]]; then
+    echo "Root privileges required."
+    exit 1
+fi
+
+if [ ! -x "$(command -v curl)" ]; then
+  echo "Could not find curl. Installing curl..."
+  apt-get install -y curl
+fi
+
+
 if [[ $# -eq 0 ]]; then
-  echo "You must pass a image file"
-  exit
+  RASPIAN_OS_VERSION=$(curl -s https://downloads.raspberrypi.org/raspios_lite_arm64/os.json | sed -n 's/.*"version": "\(.*\)"$/\1/p')
+  RASPIAN_OS_RELEASE_DATE=$(curl -s https://downloads.raspberrypi.org/raspios_lite_arm64/os.json | sed -n 's/.*"release_date": "\(.*\)",$/\1/p')
+
+  if [[ -z "$RASPIAN_OS_VERSION" || -z "$RASPIAN_OS_RELEASE_DATE" ]]; then
+    echo "Could not determine latest Raspian OS version."
+    exit 1
+  fi
+
+  RASPIAN_OS_FILE="$RASPIAN_OS_RELEASE_DATE-raspios-$RASPIAN_OS_VERSION-arm64-lite.img"
+
+  if [ ! -f "$RASPIAN_OS_FILE" ]; then
+    echo "Downloading $RASPIAN_OS_FILE.xz from https://downloads.raspberrypi.org/."
+    curl -s "https://downloads.raspberrypi.org/raspios_lite_arm64/images/raspios_lite_arm64-$RASPIAN_OS_RELEASE_DATE/$RASPIAN_OS_FILE.xz" -o "$RASPIAN_OS_FILE.xz"
+    echo "Unpacking $RASPIAN_OS_FILE.xz"
+    xz -d "$RASPIAN_OS_FILE.xz"
+  fi
 fi
 
-if [[ $# -eq 2 ]]; then
-  64BIT=1
+if [ ! -f "$RASPIAN_OS_FILE" ]; then
+  echo "Could not find Raspian OS image."
+  exit 1
 fi
-exit
-OFFSET=$(fdisk -l $1 | awk '/^[^ ]*1/{ print $2*512 }')
+
+if [ ! -x "$(command -v qemu-img)" ]; then
+  echo "Could not find qemu-img. Installing qemu-img..."
+  apt-get install -y qemu-utils
+fi
+
+if [ ! -x "$(command -v parted)" ]; then
+  echo "Could not find parted. Installing parted..."
+  apt-get install -y parted
+fi
+
+echo "Resizing $RASPIAN_OS_FILE to 8 GB."
+qemu-img resize "$RASPIAN_OS_FILE" 8G
+
+echo "Resizing root partition to fill new space."
+parted -s "$RASPIAN_OS_FILE" resizepart 2 100%
+
+echo "Mounting $RASPIAN_OS_FILE"
+OFFSET=$(fdisk -l "$RASPIAN_OS_FILE" | awk '/^[^ ]*1/{ print $2*512 }')
 mkdir boot
-sudo mount -o loop,offset=$OFFSET $1 boot
+sudo mount -o loop,offset="$OFFSET" "$RASPIAN_OS_FILE" boot
 
-read -rsp "Please enter password for pi user: " PASSWORD
+read -rp "Please enter hostname: " HOSTNAME
 echo
-PASS=$(echo $PASSWORD |  openssl passwd -6 -stdin)
-echo "pi:$PASS" > userconf.txt
+read -rp "Please enter username: " USERNAME
+echo
+read -rsp "Please enter password for $USERNAME user: " PASSWORD
+echo
+PASS=$(echo "$PASSWORD" |  openssl passwd -6 -stdin)
+echo "$USERNAME:$PASS" > userconf.txt
 
 sudo cp userconf.txt boot/userconf.txt
 sudo sync
 
 sudo umount boot
 rmdir boot
+
+export HOSTNAME
+export USERNAME
 export PASSWORD
-if [[ 64BIT eq 1 ]]; then
-  ./create-image -64 $1
-else
-  ./create-image $1
+
+
+if [ ! -x "$(command -v expect)" ]; then
+  echo "Installing expect"
+  apt-get install -y expect
 fi
+
+./create-image "$RASPIAN_OS_FILE"
